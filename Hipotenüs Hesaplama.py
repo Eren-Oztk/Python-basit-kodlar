a5659f61c214090cf333e26d26f9ead3adae8443
Eren-Oztk <59086989+Eren-Oztk@users.noreply.github.com>
2020-12-28 01:07:43 +0300

    Add files via upload

---
+++ Hipotenüs Hesaplama.py
@@ -0,0 +1,10 @@
+print("""
+*****HİPOTENÜS HESAPLAMA*****""")
+
+a=float(input("Verilen üçgenin birinci kenarını yazınız:"))
+b=float(input("Verilen üçgenin ikinci kenarını yazınız:"))
+c=(a**2)+(b**2)
+d=c**0.5
+print("Hipotenüs=",d)
+
+
